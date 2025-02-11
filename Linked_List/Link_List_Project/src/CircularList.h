854912baeabc4c7986dd37fc438d063fc9004a5a
Trostin Geles <trostinrocks@icloud.com>
2025-02-11 03:29:35 -0500

    insert and remove function for front update

--- Linked_List/Link_List_Project/src/CircularList.h
+++ Linked_List/Link_List_Project/src/CircularList.h
@@ -21,32 +21,49 @@ struct Node
 
 
 template <class T>
-class LinkedList
+class CircularList
 {
     int num_nodes;
-    Node<T>* head;
+    Node<T>* tail; // establishes the last node in the list so when edited the program won't enter an infinite loop
 
 public:
-    LinkedList() {
-        num_nodes = 0;
-        head = nullptr;
+    -CircularList() {
+        while (!is_empty()) {
+            remove_from_front();
+        }
     }
 
-    void insert_at_front(T cargo) {
-        Node<T>* front = new Node<T>(cargo, head);
-        head = front;
-        num_nodes++;
+    void insert_at_front(T cargo) 
+    {
+      Node<T>* new_node = new Node<T>(cargo);
+      if (tail == nullptr) {
+          tail = new_node;
+          tail->next = tail; // Circular link
+      } else {
+          new_node->next = tail->next;
+          tail->next = new_node;
+      }
+      num_nodes++;
     }
 
-    T remove_from_front() {
-        if (head == nullptr)
-            throw runtime_error("Can't remove from and empty list!");
-        T cargo = head->cargo;
-        Node<T>* front = head;
-        head = head->next;
-        delete front;
-        num_nodes--;
-        return cargo;
+    T remove_from_front() 
+    {
+      if (tail == nullptr) { //checks if the tail is null, if so, it throws an error
+          throw runtime_error("Can't remove from an empty list!");
+      }
+
+      Node<T>* front = tail->next;
+      T cargo = front->cargo;
+
+      if (tail == front) { // edge case if there is only one node in the list
+          tail = nullptr;
+      } else {
+          tail->next = front->next;
+      }
+
+      delete front;
+      num_nodes--;
+      return cargo;
     }
 
     string to_string() const
