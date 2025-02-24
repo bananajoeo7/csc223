ce60e57058d55c867f768bedee53d9becbb9127d
Trostin Geles <trostinrocks@icloud.com>
2025-02-24 02:34:53 -0500

    card test created and tested succesfully

--- Stack/src/test_stacks.cpp
+++ Stack/src/test_stacks.cpp
@@ -35,3 +35,35 @@ TEST_CASE("Test basic stack operations on stack of strings") {
     CHECK(stack.pop() == "cheese");
     CHECK(stack.empty() == true);
 }
+
+struct Card {
+    string suit;
+    string rank;
+
+    bool operator == (const Card& other) const {
+        return suit == other.suit && rank == other.rank;
+    }
+};
+
+TEST_CASE("Test stack operations with Cards") {
+    Stack<Card> stack;
+    CHECK(stack.empty() == true);
+
+    Card ace_spades = {"Spades", "Ace"};
+    Card king_hearts = {"Hearts", "King"};
+    Card queen_diamonds = {"Diamonds", "Queen"};
+    
+    stack.push(ace_spades);
+    CHECK(stack.top() == ace_spades);
+    CHECK(stack.empty() == false);
+    stack.push(king_hearts);
+    CHECK(stack.top() == king_hearts);
+    stack.push(queen_diamonds);
+    CHECK(stack.top() == queen_diamonds);
+    CHECK(stack.pop() == queen_diamonds);
+    CHECK(stack.top() == king_hearts);
+    CHECK(stack.pop() == king_hearts);
+    CHECK(stack.top() == ace_spades);
+    CHECK(stack.pop() == ace_spades);
+    CHECK(stack.empty() == true);
+}
