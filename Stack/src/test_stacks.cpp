7db6f9702eb397ff4c0ae29e9577ae26e972efca
Trostin Geles <trostinrocks@icloud.com>
2025-02-24 02:04:33 -0500

    tests added and includes set up

---
+++ Stack/src/test_stacks.cpp
@@ -0,0 +1,37 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <string>
+#include "Stack.h"
+using namespace std;
+
+TEST_CASE("Test basic stack operations on stack of ints") {
+    Stack<int> stack;
+    stack.push(9);
+    CHECK(stack.top() == 9);
+    stack.push(11);
+    CHECK(stack.top() == 11);
+    stack.push(42);
+    CHECK(stack.top() == 42);
+    CHECK(stack.pop() == 42);
+    CHECK(stack.top() == 11);
+    CHECK(stack.empty() == false);
+    stack.pop();
+    CHECK(stack.pop() == 9);
+    CHECK(stack.empty() == true);
+}
+
+TEST_CASE("Test basic stack operations on stack of strings") {
+    Stack<string> stack;
+    stack.push("cheese");
+    CHECK(stack.top() == "cheese");
+    stack.push("anchovies");
+    CHECK(stack.top() == "anchovies");
+    stack.push("onions");
+    CHECK(stack.top() == "onions");
+    CHECK(stack.pop() == "onions");
+    CHECK(stack.top() == "anchovies");
+    CHECK(stack.empty() == false);
+    stack.pop();
+    CHECK(stack.pop() == "cheese");
+    CHECK(stack.empty() == true);
+}
