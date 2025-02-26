e25ff230a695802679f30f01cac1801f56d993a0
Trostin Geles <trostinrocks@icloud.com>
2025-02-26 08:45:40 -0500

    function creation and addition of Jeff' split_on_spaces

--- Stack/src/Stack.h
+++ Stack/src/Stack.h
@@ -45,6 +45,38 @@ public:
     size_t size() const {
         return top_item + 1;
     }
+
+    vector<string> split_on_spaces(const string& s)
+    {
+        vector<string> tokens;
+        string token;
+        bool in_token = false;
+
+        for (char ch : s) {
+            if (ch != ' ') {
+                // if ch not a space, add it to current token
+                token += ch;
+                in_token = true;
+            } else if (in_token) {
+            // if space and in a token, push token to tokens list 
+                tokens.push_back(token);
+                token.clear(); // clear token for next word
+                in_token = false;
+            }
+        }
+
+        // handle last token if string doesn't end with a space
+        if (in_token)
+            tokens.push_back(token);
+
+        return tokens;
+    }
+
+
+    bool is_valid_infix_expr()
+    {
+
+    }
 };
 
 #endif // STACK_H
